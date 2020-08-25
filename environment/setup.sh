7b2ed111677fdc5f0169fdf0862b0111f2f30cd7
marwai <mtse@spartaglobal.com>
2020-08-25 20:26:32 +0100

    working code

---
+++ environment/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# runs docker-compose up -detached
+docker-compose up -d
+
+#winpty
+
+
+# Exec goes into the container ID and run command
+# docker ps -l give us the most recent container
+# --format "{{.ID}} takes the recent ID into the execute command
+winpty docker exec -it $(docker ps -l --format "{{.ID}}") node seeds/seed.js
